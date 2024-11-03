8ea13826987b6afa4f6e480097d0a6ac8babeeaa
floriandimas <124957454+floriandimas@users.noreply.github.com>
2024-11-03 21:19:43 +0700

    Add files via upload

---
+++ function/template.php
@@ -0,0 +1,17 @@
+<?php
+
+function headerTemplate()
+{ ?>
+	<h1>SISTEM INFORMASI KEPEGAWAIAN</h1>
+	<nav>
+		<a href="dashboard.php">Beranda</a> |
+		<a href="tampil_pegawai.php">Kelola Pegawai</a> |
+		<a href="#">Kelola Divisi</a> |
+		<a href="#">Kelola Golongan</a> |
+		<a href="#">LogOut</a>
+	</nav>
+	<br />
+<?php
+}
+
+?>
